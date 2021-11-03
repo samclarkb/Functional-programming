e3ac5beb94858553ea986acf36de50363ef5345b
samclarkb <samclarkboot@planet.nl>
2021-11-03 21:31:47 +0100

    eslint download

--- api.js
+++ api.js
@@ -4,28 +4,28 @@ const dataOphalen = () => {
 	) // Dit is de link van de API
 		.then(response => response.json())
 		.then(data => {
-			gemiddelde(data)
-			sorteren(data)
-			printen(data)
+			gemiddeldeBerekenen(data)
+			sorterenVanHoogNaarLaag(data)
+			dataPrintenNaarDeConsole(data)
 		})
 		.catch(err => {
 			console.error(err) // mocht er iets niet kloppen catched de function een error
 		})
 }
 
-const gemiddelde = data => {
+const gemiddeldeBerekenen = data => {
 	data.artists.artist.forEach(data => {
 		data.average = data.playcount / data.listeners
 		// Ik deel hier het totale aantal streams door het totaal aantal verschillende listeners, zodat ik het gemiddelde aantal nummers per luisteraar krijg
 	})
 }
 
-const sorteren = data => {
+const sorterenVanHoogNaarLaag = data => {
 	// functie voor het
 	data.artists.artist.sort((a, b) => b.average - a.average) // ik doe zet b voor a, omdat ik het hoogste cijfer bovenaan de lijst wil hebben.
 }
 
-const printen = data => {
+const dataPrintenNaarDeConsole = data => {
 	data.artists.artist.forEach(data => {
 		console.log(
 			`De gemiddelde ${data.name} luisteraar heeft naar ${Math.round(
